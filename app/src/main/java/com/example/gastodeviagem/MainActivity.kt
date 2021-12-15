4556e9da0253613b65c597df1e5a0a08ba5e8693
Sergio Cabral <scastrocabral@gmail.com>
2021-12-14 22:02:32 -0300

    Validação de dados preenchidos ou não nos EditTexts

--- app/src/main/java/com/example/gastodeviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastodeviagem/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.gastodeviagem
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -16,17 +18,35 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View) {
         val id = v.id
-        if (id == R.id.bt_calcular){
+        if (id == R.id.bt_calcular) {
             calculate()
         }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun calculate() {
-        val distance = edt_distancia.text.toString().toFloat()
-        val price = edt_preco.text.toString().toFloat()
-        val autonomy = edt_autonomia.text.toString().toFloat()
+        if (validationOk()) {
+            try {
+                val distance = edt_distancia.text.toString().toFloat()
+                val price = edt_preco.text.toString().toFloat()
+                val autonomy = edt_autonomia.text.toString().toFloat()
 
-        val totalValue = (distance * price) / autonomy
-        tv_resultado_do_valor.text = "R$ ${"%.2f".format(totalValue)}"
+                val totalValue = (distance * price) / autonomy
+                if (totalValue == Float.POSITIVE_INFINITY || totalValue == Float.NEGATIVE_INFINITY){
+                    throw ArithmeticException()
+                }
+                tv_resultado_do_valor.text = "R$ ${"%.2f".format(totalValue)}"
+            } catch (ae: ArithmeticException){
+                Toast.makeText(this, getString(R.string.campos_validos), Toast.LENGTH_LONG).show()
+            }
+        } else {
+            Toast.makeText(this, getString(R.string.preencher_campos), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun validationOk(): Boolean {
+        return (edt_distancia.text.toString() != ""
+                && edt_preco.text.toString() != ""
+                && edt_autonomia.text.toString() != "")
     }
 }
